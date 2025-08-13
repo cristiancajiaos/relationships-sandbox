ceb02c35686e2d10d707cfc5cedddfdee4ccd1c6
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-13 17:04:32 -0400

    Primer commit

---
+++ src/main/java/com/example/relationshipssandbox/RelationshipsSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.relationshipssandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsSandboxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RelationshipsSandboxApplication.class, args);
+  }
+
+}
